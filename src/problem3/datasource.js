188f11ede8dc1ca361dbe0b28730772094e7218b
Wee Chin Wei, Bryan <bryan.wee@u.yale-nus.edu.sg>
2023-01-18 01:01:37 +0800

    updated readme, included docs for datasource

--- src/problem3/datasource.js
+++ src/problem3/datasource.js
@@ -3,18 +3,23 @@ class Listing {
   constructor(price) {
     this.token = price.pair.substring(0, 3);
     this.counter = price.pair.substring(3);
+    // returns the token or counter currency as string
     this.buy = price.buy;
+    // returns the price to buy as int
     this.sell = price.sell;
+    // returns the price to sell as int
     this.pair = price.pair;
+    // returns the token and counter as a string pair
   }
-
   mid = () => {
     return (this.buy + this.sell) / 2;
   };
+  // returns the mid point between price to buy and sell as int
 
   quote = () => {
     return this.counter;
   };
+  // returns the counter currency as string
 }
 
 class Datasource {
